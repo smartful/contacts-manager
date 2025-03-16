b8648fb1c873878ff6f1b39e9cb47de147ab84ef
smartful <remi_rodrigues@hotmail.com>
2025-03-16 01:05:57 +0100

    Add menu to index page

---
+++ menu.php
@@ -0,0 +1,7 @@
+<div id="menu">
+   <div class="element_menu">
+     <h3>Contacts Manager</h3>
+      <a href="index.php">Home</a>
+      <a href="registerForm.php">Inscription</a>
+   </div>
+</div>
