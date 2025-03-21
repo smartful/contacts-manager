1f40c4645bfc016f6e7b4a0b7a520d76bd302219
smartful <remi_rodrigues@hotmail.com>
2025-03-21 05:03:33 +0100

    Add Contact Form

---
+++ public/contacts/addContact.php
@@ -0,0 +1,77 @@
+<?php
+define('ROOT', dirname(__DIR__, 2));
+require(ROOT."/layout/layoutFunctions.php");
+session_start();
+echo htmlHead("Formulaire d'ajout", "../style");
+// include(ROOT."/utils/connexion_db.php");
+// $cities = $bdd->query('SELECT * FROM cities ORDER BY city_name;');
+?>
+    <body>
+        <div class="container">
+            <?php include(ROOT."/layout/header.php"); ?>
+            <!-- le menu principal -->
+            <?= deconnexionMenu("../") ?>
+
+            <!-- le menu des activités -->
+            <div id="menu_right">
+                <div class="element_menu element_to_left">
+                    <h3>Activités</h3>
+                    <a href="contactList.php">Contacts</a>
+                </div>
+            </div>
+
+            <div id="corps">
+                <h1>Ajout d'un contact</h1>
+                <form method="post" action="addContactProcess.php">
+                    <fieldset>
+                        <legend>Information du contact</legend>
+                        <p>Champs sont obligatoires : *</p>
+                        <div class="group-form">
+                            <div class="form-row">
+                                <label for="firstname">Prénom*</label> 
+                                <input type="text" name="firstname" id="firstname"/>
+                            </div>
+                            <div class="form-row">
+                                <label for="lastname">Nom*</label> 
+                                <input type="text" name="lastname" id="lastname"/>
+                            </div>
+                            <div class="form-row">
+                                <label for="address_1">Adresse 1*</label> 
+                                <input type=text name="address_1" id="address_1"/>
+                            </div>
+                            <div class="form-row">
+                                <label for="address_2">Adresse 2</label> 
+                                <input type=text name="address_2" id="address_2"/>
+                            </div>
+                            <div class="form-row">
+                                <label for="city">Ville*</label> 
+                                <input
+                                    type="text"
+                                    name="city_display"
+                                    id="city_display"
+                                    placeholder="Sélectionner une ville ou entrer le code postal..."
+                                    autocomplete="off"
+                                />
+                                <input type="hidden" name="city_id" id="city_id" />
+                                <div id="suggestions" class="suggestions"></div>
+                            </div>
+                            <div class="form-row">
+                                <label for="email">E-mail*</label> 
+                                <input type=email name="email" id="email"/>
+                            </div>
+                            <div class="form-row">
+                                <label for="phone">Téléphone*</label> 
+                                <input type=phone name="phone" id="phone"/>
+                            </div>
+                        </div>
+                    </fieldset>
+                    <p>
+                        <input type="submit" value="Envoyer" class="cta_button validationButton"/>
+                    </p>
+                </form>
+            </div>
+            <script src="../js/ajax/citiesAutocomplete.js"></script>
+            <?php include(ROOT."/layout/footer.php"); ?>
+        </div>
+    </body>
+</html>
--- public/contacts/contactsList.php
+++ public/contacts/contactsList.php
@@ -5,65 +5,65 @@ session_start();
 echo htmlHead("Contacts", "../style");
 ?>
     <body>
-    <div class="container">
-        <?php include(ROOT."/layout/header.php"); ?>
-        <!-- le menu principal -->
-        <?= deconnexionMenu("../") ?>
+        <div class="container">
+            <?php include(ROOT."/layout/header.php"); ?>
+            <!-- le menu principal -->
+            <?= deconnexionMenu("../") ?>
 
-        <div id="corps">
-            <h1>Contacts</h1>
-            <h2>Ajouter un contact</h2>
-            <p>
-                <a href="addContact.php">Formulaire d'ajout</a>
-            </p>
-            <h2>Liste des contacts</h2>
-            <?php
-            // On se connecte au la SGBD Mysql
-            include(ROOT."/utils/connexion_db.php");
+            <div id="corps">
+                <h1>Contacts</h1>
+                <h2>Ajouter un contact</h2>
+                <p>
+                    <a href="addContact.php">Formulaire d'ajout</a>
+                </p>
+                <h2>Liste des contacts</h2>
+                <?php
+                // On se connecte au la SGBD Mysql
+                include(ROOT."/utils/connexion_db.php");
 
-            $contacts = $bdd->prepare("
-                SELECT contacts.id, contacts.firstname, contacts.lastname
-                FROM contacts
-                INNER JOIN users ON users.id = contacts.user_id
-                WHERE users.id = :user_id
-                AND contacts.deleted = 0;
-            ");
-            $contacts->execute([
-                "user_id"=> $_SESSION["user_id"],
-            ]);
-            $data = $contacts->fetchAll();
-            ?>
+                $contacts = $bdd->prepare("
+                    SELECT contacts.id, contacts.firstname, contacts.lastname
+                    FROM contacts
+                    INNER JOIN users ON users.id = contacts.user_id
+                    WHERE users.id = :user_id
+                    AND contacts.deleted = 0;
+                ");
+                $contacts->execute([
+                    "user_id"=> $_SESSION["user_id"],
+                ]);
+                $data = $contacts->fetchAll();
+                ?>
 
-            <table>
-                <thead>
-                    <tr>
-                        <th>Prénom</th>
-                        <th>Nom</th>
-                        <th>Détails</th>
-                        <th>Supprimer</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php for($i=0; $i < count($data); $i++) :?>
+                <table>
+                    <thead>
                         <tr>
-                            <td><?= $data[$i]["firstname"]; ?></td>
-                            <td><?= $data[$i]["lastname"]; ?></td>
-                            <td style="text-align:center;">
-                                <a href="detailCustomer.php?id=<?= $data[$i]["id"]; ?>">
-                                    <img src="../images/details.png" />
-                                </a>
-                            </td>
-                            <td style="text-align:center;">
-                                <a href="deleteCustomer.php?id=<?= $data[$i]["id"]; ?>">
-                                    <img src="../images/supprimer.png" />
-                                </a>
-                            </td>
+                            <th>Prénom</th>
+                            <th>Nom</th>
+                            <th>Détails</th>
+                            <th>Supprimer</th>
                         </tr>
-                    <?php endfor;?>
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        <?php for($i=0; $i < count($data); $i++) :?>
+                            <tr>
+                                <td><?= $data[$i]["firstname"]; ?></td>
+                                <td><?= $data[$i]["lastname"]; ?></td>
+                                <td style="text-align:center;">
+                                    <a href="detailCustomer.php?id=<?= $data[$i]["id"]; ?>">
+                                        <img src="../images/details.png" />
+                                    </a>
+                                </td>
+                                <td style="text-align:center;">
+                                    <a href="deleteCustomer.php?id=<?= $data[$i]["id"]; ?>">
+                                        <img src="../images/supprimer.png" />
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endfor;?>
+                    </tbody>
+                </table>
+            </div>
+            <?php include(ROOT."/layout/footer.php"); ?>
         </div>
-        <?php include(ROOT."/layout/footer.php"); ?>
-    </div>
     </body>
 </html>
