7de45bbe7b12aabcc8ddbaf6941da30236589a00
smartful <remi_rodrigues@hotmail.com>
2025-03-29 04:48:06 +0100

    Add environnement config file

--- utils/connexion_db.php
+++ utils/connexion_db.php
@@ -1,7 +1,12 @@
 <?php
+if (!defined('ROOT')) {
+    define('ROOT', dirname(__DIR__));
+}
+$config = parse_ini_file(ROOT . '/.env');
+
 //On se connecte au SGBD Mysql
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=contacts_manager','root','root');
+    $bdd = new PDO("mysql:host={$config['DB_HOST']};dbname={$config['DB_NAME']}", $config['DB_USER'], $config['DB_PASS']);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $error) {
     die('Error :'.$error->getMessage());
