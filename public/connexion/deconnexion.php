93f7bc0911160b771697189fc268fded8a741264
smartful <remi_rodrigues@hotmail.com>
2025-03-31 03:32:42 +0200

    fix deconnexion redirection on prod

--- public/connexion/deconnexion.php
+++ public/connexion/deconnexion.php
@@ -1,5 +1,15 @@
 <?php
+if (!defined('ROOT')) {
+    define('ROOT', dirname(__DIR__));
+}
 session_start();
-
 session_destroy();
-header('Location: /contacts_manager/public/index.php');
+
+$config = parse_ini_file(ROOT.'/.env');
+
+if ($config['ENV'] == 'prod') {
+  header('Location: /index.php');
+} else {
+  header('Location: /contacts_manager/public/index.php');
+}
+exit;
--- utils/connexion_db.php
+++ utils/connexion_db.php
@@ -2,9 +2,9 @@
 if (!defined('ROOT')) {
     define('ROOT', dirname(__DIR__));
 }
-$config = parse_ini_file(ROOT . '/.env');
+$config = parse_ini_file(ROOT.'/.env');
 
-//On se connecte au SGBD Mysql
+// On se connecte au SGBD Mysql
 try {
     $bdd = new PDO("mysql:host={$config['DB_HOST']};dbname={$config['DB_NAME']}", $config['DB_USER'], $config['DB_PASS']);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
